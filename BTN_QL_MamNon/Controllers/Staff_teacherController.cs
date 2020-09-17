063760330c38aa4b94d24949a4e5a09cc9c7f146
LAPTOP-L2FQS791\NAM BUI <bvnam98@gmail.com>
2020-09-18 01:42:38 +0700

    staff_teacher

---
+++ BTN_QL_MamNon/Controllers/Staff_teacherController.cs
@@ -0,0 +1,109 @@
+﻿using BTN_QL_MamNon.DTO;
+using BTN_QL_MamNon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BTN_QL_MamNon.Controllers
+{
+    public class Staff_teacherController : ApiController
+    {
+        // GET: api/Staff_teacher
+        public HttpResponseMessage Get()
+        {
+            QLMamNonEntities db = new QLMamNonEntities();
+            var result = db.staff_teacher;
+            return Request.CreateResponse(HttpStatusCode.OK, result.ToList());
+        }
+        // GET: api/Staff_teacher/5
+        public staff_teacherDTO Get(int id)
+        {
+            using (QLMamNonEntities db = new QLMamNonEntities())
+            {
+                staff_teacher s = db.staff_teacher.SingleOrDefault(x => x.id == id);
+                if (s != null)
+                {
+                    return new staff_teacherDTO(s.id, s.name, s.address, s.avatar, s.username, s.password, s.phone);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        // POST: api/Staff_teacher
+        public HttpResponseMessage Post([FromBody] staff_teacher obj)
+        {
+            try
+            {
+                using (QLMamNonEntities db = new QLMamNonEntities())
+                {
+                    db.staff_teacher.Add(obj);
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.Created, obj);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+        }
+
+        // PUT: api/Staff_teacher/5
+        public HttpResponseMessage Put(int id, [FromBody] staff_teacher value)
+        {
+            try
+            {
+                using (QLMamNonEntities db = new QLMamNonEntities())
+                {
+                    staff_teacher s = db.staff_teacher.SingleOrDefault(b => b.id == id);
+                    if (s != null)
+                    {
+                        s.name = value.name;
+                        s.address = value.address;
+                        s.avatar = value.avatar;
+                        s.phone = value.phone;
+                        s.username = value.username;
+                        s.password = value.password;
+                        db.SaveChanges();
+
+                        return Request.CreateResponse(HttpStatusCode.OK, new staff_teacherDTO(s.id, s.name, s.address, s.avatar, s.username,s.password,s.phone));
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+        // DELETE: api/Staff_teacher/5
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (QLMamNonEntities db = new QLMamNonEntities())
+                {
+                    staff_teacher s = db.staff_teacher.SingleOrDefault(x => x.id == id);
+                    db.staff_teacher.Remove(s);
+                    db.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+        }
+    }
+}
---
+++ BTN_QL_MamNon/DTO/staff_teacherDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTN_QL_MamNon.DTO
+{
+    public class staff_teacherDTO
+    {
+        long id;
+        string name;
+        string phone;
+        string username;
+        string password;
+        string address;
+        string avatar;
+
+        public staff_teacherDTO(long id, string name, string phone, string username, string password, string address, string avatar)
+        {
+            this.id = id;
+            this.name = name;
+            this.phone = phone;
+            this.username = username;
+            this.password = password;
+            this.address = address;
+            this.avatar = avatar;
+        }
+
+        public long Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public string Address { get => address; set => address = value; }
+        public string Avatar { get => avatar; set => avatar = value; }
+    }
+}
