0b5d512be7851508e6aad2c1f17b5282438503d0
HuongHuong29102019 <70678478+HuongHuong29102019@users.noreply.github.com>
2020-09-18 00:05:31 +0700

    aa

--- BTN_QL_MamNon/Controllers/HomeController.cs
+++ BTN_QL_MamNon/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace BTN_QL_MamNon.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index() 
+        public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
